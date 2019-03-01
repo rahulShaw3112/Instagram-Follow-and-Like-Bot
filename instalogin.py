7c6f4397f8ae5c81b5f4e117d80c63fa6ec440e7
Rahul Shaw <rahul.shaw.274@gmail.com>
2019-03-01 20:33:10 +0530

    commented the steps to follow

--- instalogin.py
+++ instalogin.py
@@ -76,25 +76,25 @@ class InstagramBot:
         self.driver.close()
 
 
-# steps to follow:
-# Update username and password in config.json
-# create a InstagramBot object            
-bot = InstagramBot()
+# # steps to follow:
+# # Update username and password in config.json
+# # create a InstagramBot object            
+# bot = InstagramBot()
 
-# initialize the driver
-bot.setDriver()
+# # initialize the driver
+# bot.setDriver()
 
-# login as the bot
-bot.login()
+# # login as the bot
+# bot.login()
 
-# either follow any profile giving only the user id which is part of: 
-# profile url: https://www.instagram.com/rahulshaw274/
-# userid: rahulshaw274
-profileToFollow = 'rahulshaw274'
-bot.follow(profileToFollow)
+# # either follow any profile giving only the user id which is part of: 
+# # profile url: https://www.instagram.com/rahulshaw274/
+# # userid: rahulshaw274
+# profileToFollow = 'rahulshaw274'
+# bot.follow(profileToFollow)
 
-# or like every photo of a pofile
-profileToLike = 'rahulshaw274'
-bot.likePhoto(profileToLike)
+# # or like every photo of a pofile
+# profileToLike = 'rahulshaw274'
+# bot.likePhoto(profileToLike)
 
 
